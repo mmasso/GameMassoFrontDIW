ebffe19f627a563c8f81632f1b0891b3d95cd5ce
mmasso <mmasso@cifpfbmoll.eu>
2020-12-01 23:53:53 +0100

    feat: añadir estructura

---
+++ src/characters.js
@@ -0,0 +1,5 @@
+$(document).ready(() => {
+    //Load the HTML contents of the header into <header>
+    $("header").load("resources/menu.html");
+    $("footer").load("resources/footer.html");
+});
